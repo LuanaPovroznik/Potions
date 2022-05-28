881a51b00cbfb347dbee43e6dd1926835d6d398b
Matheus Portes <matheusportes12@gmail.com>
2022-05-28 18:31:11 -0300

    feat: added navigation bar

---
+++ src/navigation_bar.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="css/navigation_bar_style.css">
+<ol>
+    <li><p><span>LEAKY</span> CAULDRON</p></li>
+    <li style="float: right"><a href="login.php">Sou funcionário</a></li>
+    <li style="float: right"><a href="register_cliente.php">Cadastre-se</a></li>
+    <li style="float: right"><a href="login_cliente.php">Login</a></li>
+    <li style="float: right"><a href="index.php">Inicial</a></li>
+</ol>
