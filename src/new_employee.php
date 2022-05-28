8c686368314079c721d9e7b614d84d01a57888f4
Matheus Portes <matheusportes12@gmail.com>
2022-05-28 13:57:14 -0300

    wip: add new employee page

---
+++ src/new_employee.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastrar Novo Funcionário</title>
+</head>
+    <body>
+        <div class="container">
+            <div class="formulario">
+                <form action="#" name="newEmployeeForm" id="newEmployeeForm">
+                    <label for="employeeName">Nome:</label><br>
+                    <input type="text">
+                    <label for="employeeLevel">Permissão de administrador:</label><br>
+                    <input type="radio"> Sim
+                    <input type="radio"> Não
+                    <label for="employeeCPF">CPF:</label><br>
+                    <input type="text">
+                    <label for="employeeLogin">Login:</label><br>
+                    <input type="text">
+                    <label for="employeePassword">Senha:</label><br>
+                    <input type="password">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
