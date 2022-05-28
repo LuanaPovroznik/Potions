06a0bc31c97d436bccfbfe14d2460a2f3843f571
Matheus Portes <matheusportes12@gmail.com>
2022-05-28 15:33:29 -0300

    wip: add new employee working | add new potion and potion type in progress

--- src/new_employee.php
+++ src/new_employee.php
@@ -1,3 +1,6 @@
+<?php
+    include 'config.php';
+?>
 <!doctype html>
 <html lang="pt-BR">
 <head>
@@ -9,21 +12,49 @@
 </head>
     <body>
         <div class="container">
-            <div class="formulario">
-                <form action="#" name="newEmployeeForm" id="newEmployeeForm">
+            <div class="form">
+                <form action="new_employee.php" method="POST" name="newEmployeeForm" id="newEmployeeForm">
+                    <p>Cadastrar Novo Funcionário</p>
                     <label for="employeeName">Nome:</label><br>
-                    <input type="text">
+                    <input type="text" name="employeeName"><br><br>
                     <label for="employeeLevel">Permissão de administrador:</label><br>
-                    <input type="radio"> Sim
-                    <input type="radio"> Não
+                    <input type="radio" name="employeeLevel" value="Sim"> Sim<br>
+                    <input type="radio" name="employeeLevel" value="Não"> Não<br><br>
                     <label for="employeeCPF">CPF:</label><br>
-                    <input type="text">
+                    <input type="text" name="employeeCPF"><br><br>
                     <label for="employeeLogin">Login:</label><br>
-                    <input type="text">
+                    <input type="text" name="employeeLogin"><br><br>
                     <label for="employeePassword">Senha:</label><br>
-                    <input type="password">
+                    <input type="password" name="employeePassword"><br><br>
+                    <input type="submit" name="addButton" value="Cadastrar">
                 </form>
             </div>
         </div>
     </body>
 </html>
+
+<?php
+    if(@$_REQUEST['addButton'] == "Cadastrar") {
+        @$employeeName = $_POST["employeeName"];
+        @$employeeLevel = $_POST["employeeLevel"];
+        @$employeeCPF = $_POST["employeeCPF"];
+        @$employeeLogin = $_POST["employeeLogin"];
+        @$employeePassword = md5($_POST['employeePassword']);
+
+        if(@$employeeLevel == "Sim"){
+            $isAdm = 1;
+        } else {
+            $isAdm = 0;
+        }
+
+        $sql = "INSERT INTO funcionario (isAdm, nome, cpf, login, password) 
+                    VALUES ('$isAdm', '$employeeName', '$employeeCPF', '$employeeLogin', '$employeePassword')";
+
+
+        if (mysqli_query($con, $sql)) {
+            echo "Cadastrado com sucesso.";
+        } else {
+            "Erro ao cadastrar";
+        }
+    }
+?>
