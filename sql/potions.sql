0176293da813b713009a4c13178958650ed58c67
LuanaPovroznik <luanapovroznik@gmail.com>
2022-05-28 13:18:30 -0300

    correction login and sql for import

---
+++ sql/potions.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 28-Maio-2022 às 18:14
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `potions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(5) NOT NULL,
+  `nome` varchar(80) NOT NULL,
+  `login` varchar(60) NOT NULL,
+  `password` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id` int(5) NOT NULL,
+  `isAdm` int(1) NOT NULL,
+  `nome` varchar(80) NOT NULL,
+  `cpf` varchar(25) NOT NULL,
+  `login` varchar(60) NOT NULL,
+  `password` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `potion`
+--
+
+CREATE TABLE `potion` (
+  `id` int(5) NOT NULL,
+  `nome` varchar(80) NOT NULL,
+  `preco` varchar(20) NOT NULL,
+  `tipo` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo`
+--
+
+CREATE TABLE `tipo` (
+  `id` int(5) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `efeito` varchar(180) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+CREATE TABLE `venda` (
+  `id` int(5) NOT NULL,
+  `data` date NOT NULL,
+  `total` varchar(20) NOT NULL,
+  `produto` int(5) NOT NULL,
+  `cliente` int(5) NOT NULL,
+  `funcionario` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `potion`
+--
+ALTER TABLE `potion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tipo` (`tipo`);
+
+--
+-- Índices para tabela `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `funcionario` (`funcionario`),
+  ADD KEY `cliente` (`cliente`),
+  ADD KEY `produto` (`produto`);
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `potion`
+--
+ALTER TABLE `potion`
+  ADD CONSTRAINT `potion_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipo` (`id`);
+
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `venda_ibfk_1` FOREIGN KEY (`funcionario`) REFERENCES `funcionario` (`id`),
+  ADD CONSTRAINT `venda_ibfk_2` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `venda_ibfk_3` FOREIGN KEY (`produto`) REFERENCES `potion` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/login.php
@@ -0,0 +1,53 @@
+<html>
+
+<head>
+    <title>Login</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<?php
+include ('config.php');
+session_start(); 
+
+if (@$_REQUEST['button']=="Login")
+{
+	$login = $_POST['login'];
+	$password = md5($_POST['password']);
+	
+	$query = "SELECT * FROM funcionario WHERE login = '$login' AND password = '$password' ";
+	$result = mysqli_query($con, $query);
+	while ($coluna=mysqli_fetch_array($result)) 
+	{
+		$_SESSION["id"]= $coluna["id"]; 
+		$_SESSION["login"] = $coluna["login"]; 
+		$_SESSION["isAdm"] = $coluna["isAdm"];
+
+		$cargo = $coluna['isAdm'];
+		if($isAdm == "0"){ 
+			header("Location: menu.php"); 
+			exit; 
+		}
+		
+		if($isAdm == "1"){ 
+			header("Location: menu.php"); 
+			exit; 
+		}
+	}
+	
+}
+?>
+
+<body>
+<h2 style="text-align: center"><span>Potions</span></h2>
+    <div class="container">
+        <h1>Seja <span>bem vindo</span></h1>
+        <form class="user" action=# method=post>
+            <input type="text" aria-describedby="emailHelp" placeholder="Enter username" name="login"><br><br>
+            <input type="password" id="password" placeholder="Password" name="password"> <br><br>
+            <a href="register_user.php"><button type="button" class="button">Crie sua conta</button></a>
+            <input type="submit" name="button" value="Login" class="button"><br>
+        </form>
+        <a href="posts_page_logged_out.php"><button type="button" class="button">Visualizar produtos</button></a>
+    </div>
+</body>
+</html>
