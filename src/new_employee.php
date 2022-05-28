68a80c7a84e500b9e865a77105020dc882e3b9ec
Matheus Portes <matheusportes12@gmail.com>
2022-05-28 16:14:40 -0300

    wip: add new employee working | check cpf fully working | add new potion and potion type in progress

--- src/new_employee.php
+++ src/new_employee.php
@@ -21,12 +21,40 @@
                     <input type="radio" name="employeeLevel" value="Sim"> Sim<br>
                     <input type="radio" name="employeeLevel" value="Não"> Não<br><br>
                     <label for="employeeCPF">CPF:</label><br>
-                    <input type="text" name="employeeCPF"><br><br>
+                    <input type="text" name="employeeCPF" onfocusout="checkCPF()" id="employeeCPF"><br>
+                    <script>
+                        function checkCPF(){
+                            fetch("http://localhost/Trabalho2Bimestre/src/check_cpf.php", {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                                },
+                                body: `cpf=${document.getElementById("employeeCPF").value}`,
+                            }).then((response) => response.text())
+                                .then((res) => {
+                                    document.getElementById("result_cpf").innerHTML = res;
+                                    letRegisterCPF();
+                                });
+                        }
+                    </script>
+                    <p id="result_cpf" style="font-style: italic; font-size: small"></p>
+                    <script>
+                        function letRegisterCPF(){
+                            cpfAvaiable = document.getElementById("result_cpf").innerHTML.valueOf();
+                            if(cpfAvaiable == "CPF Inválido." || cpfAvaiable == "Obrigatório CPF com 11 dígitos."){
+                                document.querySelector('#addButton').disabled = true;
+                                document.getElementById("result_cpf").style.color="red";
+                            } else {
+                                document.getElementById("result_cpf").style.color="green";
+                                document.querySelector('#addButton').disabled = false;
+                            }
+                        }
+                    </script>
                     <label for="employeeLogin">Login:</label><br>
                     <input type="text" name="employeeLogin"><br><br>
                     <label for="employeePassword">Senha:</label><br>
                     <input type="password" name="employeePassword"><br><br>
-                    <input type="submit" name="addButton" value="Cadastrar">
+                    <input type="submit" name="addButton" value="Cadastrar" id="addButton">
                 </form>
             </div>
         </div>
