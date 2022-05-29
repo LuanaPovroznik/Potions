755731aa36d02763bd011d7b04b28ae272fa8a5c
LuanaPovroznik <luanapovroznik@gmail.com>
2022-05-29 14:44:48 -0300

    fix employee login

--- src/login.php
+++ src/login.php
@@ -5,6 +5,7 @@
 	</head>
 	<?php
 	include ('config.php');
+	include 'navigation_bar.php';
 	session_start(); 
 
 	if (@$_REQUEST['button']=="Login")
@@ -21,12 +22,12 @@
 			$_SESSION["isAdm"] = $coluna["isAdm"];
 
 			$cargo = $coluna['isAdm'];
-			if($isAdm == "0"){ 
-				header("Location: menu.php"); 
+			if($cargo == "0"){ 
+				header("Location: menu_employee.php"); 
 				exit; 
 			}
-			if($isAdm == "1"){ 
-				header("Location: menu.php"); 
+			if($cargo == "1"){ 
+				header("Location: menu_employee.php"); 
 				exit; 
 			}
 		}
@@ -41,10 +42,8 @@
 			<form class="user" action=# method=post>
 				<input type="text" aria-describedby="emailHelp" placeholder="Enter username" name="login"><br><br>
 				<input type="password" id="password" placeholder="Password" name="password"> <br><br>
-				<a href="new_employee.php"><button type="button" class="button">Funcionario!</button></a>
 				<input type="submit" name="button" value="Login" class="button"><br>
 			</form>
-			<a href="menu.php"><button type="button" class="button">Visualizar produtos</button></a>
 		</div>
 	</body>
 </html>
