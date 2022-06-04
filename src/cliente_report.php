2cb7061d68d06e4a6b33c46c0d30b6623ef05088
LuanaPovroznik <luanapovroznik@gmail.com>
2022-06-04 14:34:22 -0300

    relatorios sem css

--- src/cliente_report.php
+++ src/cliente_report.php
@@ -4,9 +4,6 @@
     <?php include ('config.php'); 
     include 'logged_user_nav_bar.php';
     include 'host.php'; ?>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js"></script>
 </head>
     <body>
         <?php        
@@ -33,13 +30,18 @@
             fclose($myfile); 
         }
         ?>
-        
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.css" />
-        <table id="tblUser">
+        <input 
+         type="text" 
+         id="myInput" 
+         onkeyup="myFunction()" 
+         placeholder="Procure pelo cliente" 
+         title="Filtra a tabela">
+       
+        <table id="myTable">
             <thead>
-                <th>Nome</th>
-                <th>Login</th>
-                <th>CPF</th>
+                <th onclick="sortTable(0)">Nome</th>
+                <th onclick="sortTable(1)">Login</th>
+                <th onclick="sortTable(2)">CPF</th>
             </thead>
             <tbody>
                 <?php if(!empty($clientes)) { ?>
@@ -52,16 +54,67 @@
                     <?php } ?>
                 <?php } ?>
             </tbody>
+            <tfoot>
+                <p id="totalRegister"></p>
+            </tfoot>
         </table>
         <form action="#" method="POST">
             <a href="vendas.txt" download><button value="Exportar arquivo" name="botao">Exportar arquivo</button></a>
+            <a href="vendas_pdf.php" target="_blank"><input type="button" value="Imprimir"/>
         </form>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js"></script>
         <script>
-        jQuery(document).ready(function($) {
-            $('#tblUser').DataTable();
-        } );
+            const myFunction = () => {
+                const trs = document.querySelectorAll('#myTable tr:not(.header)')
+                const filter = document.querySelector('#myInput').value
+                const regex = new RegExp(filter, 'i')
+                const isFoundInTds = td => regex.test(td.innerHTML)
+                const isFound = childrenArr => childrenArr.some(isFoundInTds)
+                const setTrStyleDisplay = ({ style, children }) => {
+                    style.display = isFound([
+                    ...children 
+                ]) ? '' : 'none' 
+                }
+  
+                trs.forEach(setTrStyleDisplay)
+            }
+            var x = document.getElementById("myTable").rows.length;
+            document.getElementById("totalRegister").innerHTML = "Há "+x+" registros";
+            function sortTable(n) {
+            var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+            table = document.getElementById("myTable");
+            switching = true;
+            dir = "asc";
+            while (switching) {
+                switching = false;
+                rows = table.rows;
+                for (i = 1; i < (rows.length - 1); i++) {
+                shouldSwitch = false;
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                    }
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                    }
+                }
+                }
+                if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                switchcount ++;
+                } else {
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+                }
+            }
+            }
         </script>
     </body>
 </html>
